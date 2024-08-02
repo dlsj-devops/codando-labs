55ac38de91ac4def8005a4fc2c15c1774e518b21
Daniel de Lima <dlima@tre-pa.jus.br>
2024-08-02 19:32:22 -0300

    Recuperando o acesso ao storage

---
+++ java/mailservice/src/main/java/com/example/mailservice/config/OSClientConfiguration.java
@@ -0,0 +1,28 @@
+package com.example.mailservice.config;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.oracle.bmc.ConfigFileReader;
+import com.oracle.bmc.Region;
+import com.oracle.bmc.auth.ConfigFileAuthenticationDetailsProvider;
+import com.oracle.bmc.objectstorage.ObjectStorage;
+import com.oracle.bmc.objectstorage.ObjectStorageClient;
+
+@Configuration
+public class OSClientConfiguration {
+    String configurationFilePath = "/.oci/config";
+    String profile = "DEFAULT";
+
+    public ObjectStorage getObjectStorage() throws IOException {
+        Path currentRelativePathe = Paths.get("");
+
+        final ConfigFileReader.ConfigFile configFile = ConfigFileReader.parse(currentRelativePathe.toAbsolutePath() + configurationFilePath, profile); 
+        final ConfigFileAuthenticationDetailsProvider provider = new ConfigFileAuthenticationDetailsProvider(configFile);
+
+        return ObjectStorageClient.builder().region(Region.SA_SAOPAULO_1).build(provider);
+    }
+}
