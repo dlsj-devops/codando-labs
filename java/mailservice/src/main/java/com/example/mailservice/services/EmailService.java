9db6317f00ffac5f1587f9d5aa84a0aa012eade3
Daniel de Lima <dlima@tre-pa.jus.br>
2024-08-03 16:22:18 -0300

    Serviço de email

---
+++ java/mailservice/src/main/java/com/example/mailservice/services/EmailService.java
@@ -0,0 +1,38 @@
+package com.example.mailservice.services;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+    
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void sendReport(String content, String to) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom(from);
+            helper.setText(content, true);
+            int currentMonth = new Date().getMonth();
+            helper.setSubject("Monthly Report: " + currentMonth);
+            helper.setTo(to);
+
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+   
+}
